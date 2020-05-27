ccb6f63f409d188212f893def0f624befb875985
DeAndre Johnson <dre@phreshr.com>
2020-05-27 13:52:35 +0800

    update verifyToken middleware

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -16,19 +16,6 @@ export const timer: Middleware = async (ctx, next) => {
   ctx.response.headers.set("X-Response-Time", `${ms}ms`);
 };
 
-// export const timer: Middleware = async (ctx, next) => {
-//   const start = performance.now();
-//   await next();
-//   const duration = performance.now() - start;
-//   ctx.response.headers.set("X-Response-Time", `${duration}ms`);
-// };
-
-// export const logger: Middleware = async (ctx, next) => {
-//   const duration = ctx.response.headers.get("X-Response-Time");
-//   await next();
-//   console.log(`${ctx.request.method} ${ctx.request.url} - ${duration}ms`);
-// };
-
 export const errorHandler: Middleware = async (ctx, next) => {
   try {
     await next();
@@ -40,39 +27,45 @@ export const errorHandler: Middleware = async (ctx, next) => {
 };
 
 export const verifyToken = async (
-  ctx: Context<{ user: Omit<User, "password"> | null }>,
+  { request, response, state }: Context,
   next: () => Promise<void>
 ) => {
   const token =
-    ctx.request.headers.get("authorization")?.split("bearer ")?.[1] || "";
+    request.headers.get("authorization")?.split("bearer ")?.[1] || "";
 
   if (!token) {
-    ctx.response.status = 400;
-    ctx.response.body = {
+    response.status = 401;
+    response.body = {
       msg: "Access Denied!",
     };
     return;
   }
+
   try {
     const verified = await validateJwt(token, config.JWT_SECRET, {
       isThrowing: false,
     });
 
     if (!verified) {
-      ctx.state.user = null;
+      state.user = null;
     }
 
     const user = await UserModel.findById(verified?.payload?.id! as string);
 
     if (!user) {
-      ctx.state.user = null;
+      state.user = null;
+      response.status = 401;
+      response.body = {
+        msg: "Access Denied!",
+      };
+      return;
     }
 
-    ctx.state.user = user;
+    state.user = user;
     await next();
   } catch (error) {
     console.log(error);
-    ctx.response.status = 500;
-    ctx.response.body = { error: "Server error" };
+    response.status = 500;
+    response.body = { error: "Server error" };
   }
 };
